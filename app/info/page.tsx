c1fca800a85927b2b0fa060aef374c42a8aed30a
Lars Desmet <volltixR200@gmail.com>
2024-06-17 23:17:05 +0200

    added php programming language

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -41,9 +41,12 @@ export default function Home() {
                         <li className="mb-1 flex items-center">HTML & CSS</li>
                         <li className="mb-1 flex items-center">Node.js</li>
                         <li className="mb-1 flex items-center">Python</li>
+                        <li className="mb-1 flex items-center">Basic PHP</li>
+
                     </ul>
                 </div>
-                <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+                <div
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
                     <h2 className="mb-3 text-2xl font-semibold">
                         Contact
                     </h2>
